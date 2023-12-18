df5e166ae38e7179abffdafcb86ca82249d64b46
facuzavaleta <facuzavaleta890@gmail.com>
2023-12-18 14:29:20 -0300

    modificada la primera view para que q traiga los datos al front, funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Realiza la solicitud a tu vista Django
+    axios.get('http://127.0.0.1:8000/', { params: { ajax_request: true } })
+      .then(response => {
+        // Maneja los datos recibidos
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error al obtener datos:', error);
+      });
+  }, []);
 
   return (
     <>
@@ -21,6 +36,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        {data && (
+          <p>Data from Django: {data.message}</p>
+        )}
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +47,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
